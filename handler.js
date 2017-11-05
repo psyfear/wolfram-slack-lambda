c4173bf18c8e2b84450b3bbbc5e4fb46db64692e
Jessie Espesor <psyfear@mac.com>
2017-11-05 18:57:28 +0800

    Use callbacks for slack delayed response

--- handler.js
+++ handler.js
@@ -6,16 +6,34 @@ const queryString = require('query-string');
 const fetch = require('node-fetch');
 const get = require('lodash.get');
 const transformer = require('./lib/transformer');
-
-const PREFIX = '/wolfram';
+const axios = require('axios');
 
 function callWolframAlphaApiFromQuery(query) {
   const input = encodeURIComponent(query);
   const podtitle = encodeURIComponent('Result*');
   const appid = encodeURIComponent(process.env.WOLFRAM_APP_ID);
   const url = `https://api.wolframalpha.com/v2/query?input=${input}&podtitle=${podtitle}&appid=${appid}`;
-  console.log(url);
   return fetch(url);
+}
+
+function buildReplyObject(notification) {
+  const tags = notification.tags[0];
+  const text = notification.title + ': ' + tags._text;
+  var attachments;
+  if (tags._image) {
+    attachments = [
+      {
+        'image_url': tags._image['url']
+      }
+    ]
+  }
+  
+  const reply = {
+    'response_type': 'in_channel',
+    'text': text,
+    'attachments': attachments
+  }
+  return reply;
 }
 
 module.exports.wolfram = (event, context, cb) => {
@@ -23,18 +41,28 @@ module.exports.wolfram = (event, context, cb) => {
   const message = queryString.parse(event.body);
   console.log(message);
   const messageText = message.text
-
+  const responseUrl = message.response_url
   if (!message || !messageText) {
     console.log(JSON.stringify(event));
     return cb(new Error('Invalid event'));
   }
 
-  function reply(notification) {
-    const reply = {
-      'response_type': 'in_channel',
-      'text': notification,
+  function delayedResponse(notification) {
+    console.log('responseUrl:' + responseUrl);
+    console.log(notification);
+    const reply = buildReplyObject(notification);
+    
+    if (responseUrl) {
+      axios.post(responseUrl, reply)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      return cb(null, reply);
     }
-    return cb(null, reply);
   }
 
   const query = messageText.trim();
@@ -42,6 +70,8 @@ module.exports.wolfram = (event, context, cb) => {
   callWolframAlphaApiFromQuery(query)
     .then(result => result.text())
     .then(body => transformer.transform(body, query))
-    .then(notification => reply(notification))
+    .then(notification => delayedResponse(notification))
     .catch(err => cb(err));
+  
+  return cb(null, { 'response_type': 'ephemeral', 'text': 'Executing..'});
 };
